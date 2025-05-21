7a5e0c6e5b350edb4bd741352b71a0876eb3687f
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2025-05-21 16:42:31 +0200

    can now get a to a game point from a pdn or play a move from a notation

--- PDN.py
+++ PDN.py
@@ -1,5 +1,6 @@
 from Checkers import Checkers
 from Move import Move
+import re
 
 
 def get_move_notation(move: Move) -> str:
@@ -19,3 +20,34 @@ def get_PDN(board: Checkers) -> str:
         pdn += f" {move}"
 
     return pdn
+
+def make_PDN_move(board: Checkers, move: str):
+    print(move)
+    for m in board.get_moves():
+        print(get_move_notation(m))
+        if move == get_move_notation(m):
+            board.make_move(m)
+            return
+
+    raise Exception("move not found")
+
+def get_board_from_PDN(pdn: str) -> Checkers:
+    board = Checkers()
+    board.init_board()
+    
+    new_turn_pattern = re.compile("^\d+\.$")
+
+    for move in pdn.split(" "):
+        if not new_turn_pattern.match(move):
+            make_PDN_move(board, move)
+
+    return board
+
+
+def main():
+    board = get_board_from_PDN("1. 35-30 20-25 2. 40-35 19-24 3. 30x19 14x23 4. 45-40 18-22 5. 50-45 23-28 6. 32x23 22-27 7. 31x22 17x19 8. 38-32 19-24 9. 43-38 16-21 10. 49-43 21-26 11. 36-31 15-20 12. 41-36 13-19 13. 47-41 12-18 14. 34-30 25x34 15. 40x29 20-25 16. 29x20 25x14 17. 45-40 19-24 18. 40-34 18-23 19. 44-40 14-19 20. 34-30 11-17")
+    board.show_board()
+
+
+if __name__ == "__main__":
+    main()
